a6f7dc5549b5403349826d9521af28432dabd682
Matheus Lima <matheusldasilva20088@gmail.com>
2020-10-09 18:51:03 -0300

    initialize deef project

---
+++ app/src/main/java/com/rightside/deef/activies/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.rightside.deef.activies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rightside.deef.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
