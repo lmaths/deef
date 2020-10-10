caf557f30643700a6508b5834df1f147b8aac633
Matheus Lima <matheusldasilva20088@gmail.com>
2020-10-10 11:51:16 -0300

    add methods to get and save products

--- app/src/main/java/com/rightside/deef/client/feed/FeedRepository.kt
+++ app/src/main/java/com/rightside/deef/client/feed/FeedRepository.kt
@@ -2,15 +2,21 @@ package com.rightside.deef.client.feed
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ListenerRegistration
+import com.rightside.deef.client.model.Product
 import com.rightside.deef.models.Post
 import io.reactivex.Observable
-import io.reactivex.rxkotlin.toObservable
 
 
 class FeedRepository(var database : FirebaseFirestore) : FeedContract.FirebaseService {
-    override fun sendPost(post: Post): Task<DocumentReference> {
-       return database.collection("posts").add(post.returnPost)
+    override fun sendPost(post: Post): Task<Void> {
+       return database.collection("posts").document(post.id).set(post.map)
+    }
+
+    override fun saveProductList(products: List<Product>) {
+        products.forEach {
+            database.collection("products").document(it.id).set(it.map)
+        }
+
     }
 
     override fun getPosts(): Observable<List<Post>> {
@@ -20,5 +26,13 @@ class FeedRepository(var database : FirebaseFirestore) : FeedContract.FirebaseSe
             }
         }
         }
+
+    override fun getProducts(productName : String): Observable<List<Product>> {
+        return Observable.create { emitter ->
+            database.collection("products").whereEqualTo("category",productName).addSnapshotListener{value, error ->
+                value?.toObjects(Product::class.java)?.let { emitter.onNext(it) }
+            }
+        }
+    }
 
 }
