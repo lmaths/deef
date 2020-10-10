a1873ffb210371e3f9820d1f021ba7391173fa3e
Matheus Lima <matheusldasilva20088@gmail.com>
2020-10-10 11:51:57 -0300

    add post atributes

--- app/src/main/java/com/rightside/deef/models/Post.kt
+++ app/src/main/java/com/rightside/deef/models/Post.kt
@@ -1,14 +1,20 @@
 package com.rightside.deef.models
 
 import androidx.annotation.Keep
+import com.google.firebase.firestore.auth.User
+import com.rightside.deef.client.model.Status
 import java.io.Serializable
 @Keep
-data class Post(val content : String = "", val isPrivate : Boolean = false, val date : String = "", val timeInMili : Long = 0) : Serializable {
-    val returnPost = hashMapOf(
+data class Post(val content : String = "", val isPrivate : Boolean = false, val date : String = "", val timeInMili : Long = 0, val status : Status = Status.PESQUISANDO, val productName : String = "", val photoProductUrl : String = "", val id: String = "",val user: com.rightside.deef.models.User = User("", "")) : Serializable {
+    val map = hashMapOf(
         Pair("content", content),
         Pair("isPrivate", isPrivate),
         Pair("date", date),
-        Pair("timeInMili", timeInMili)
+        Pair("timeInMili", timeInMili),
+        Pair("status", status),
+        Pair("photoProductUrl", photoProductUrl),
+        Pair("id", id),
+        Pair("user", user.map)
     )
 
 }
